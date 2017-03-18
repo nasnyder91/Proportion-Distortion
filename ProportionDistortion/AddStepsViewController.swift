a2770cc472ca0caeeeb0a49739bfffa3893ce61c
Dingy Pumba <dingypumba@dingys-imac.local>
2017-03-18 13:06:42 -0400

    Added AddStepsViewController

---
+++ ProportionDistortion/AddStepsViewController.swift
@@ -0,0 +1,42 @@
+//
+//  AddStepsViewController.swift
+//  ProportionDistortion
+//
+//  Created by Dingy Pumba on 3/18/17.
+//  Copyright © 2017 Nick Snyder. All rights reserved.
+//
+
+import UIKit
+
+class AddStepsViewController: UIViewController, UITextFieldDelegate, UINavigationControllerDelegate {
+    
+//MARK: Properties
+    @IBOutlet weak var addStepTextField: UITextField!
+    
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+//MARK: UITextViewDelegate
+    
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
